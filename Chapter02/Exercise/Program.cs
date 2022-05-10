c09f5752127d03058c6eafc296962c730ddf5c98
33073有阪圭太 <infosys@C4210>
2022-05-10 15:00:30 +0900

    問題2.1.4 完成　Console.WriteLine書式　ゼロ埋め

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -9,9 +9,18 @@ namespace Exercise {
         static void Main(string[] args) {
             var songs = new Song[] {
                 new Song("a", "aa", 246)
-                ,new Song("b", "bb", 250)
-                ,new Song("c", "cc", 230)
+                ,new Song("b", "bb", 241)
+                ,new Song("c", "cc", 240)
             };
+            PrintSongs(songs);
+        }
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+            for (int i = 0; i < songs.Length; i++) {
+                Console.WriteLine("{0} {1} {2}:{3:d2}", songs[i].Title,
+                    songs[i].ArtistName, songs[i].Length / 60, songs[i].Length % 60); 
+            }
+            //ゼロ埋め、書式設定、ゼロサプレス
         }
     }
 }
