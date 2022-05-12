d620fc32111121aefe8f412515334d7409e2fb2e
33073有阪圭太 <infosys@C4210>
2022-05-12 13:33:30 +0900

    問題2.3　完成

---
+++ Chapter02/Exercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("sales.csv");
+            var amountPerProduct = sales.GetPerProductSales();
+            foreach (var obj in amountPerProduct) {
+                Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
---
+++ Chapter02/Exercise3/SalesCounter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    //売上集計クラス
+    public class SalesCounter {
+        private IEnumerable<Sale> _sales;//csvファイルから読み込んだデータ
+
+        //コンストラクタ
+        public SalesCounter(String filePath) {
+            _sales = ReadSales(filePath);
+
+        }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        private static IEnumerable<Sale> ReadSales(String filePath) {
+            List<Sale> sales = new List<Sale>();
+            String[] lines = File.ReadAllLines(filePath);
+            foreach (String line in lines) {
+                String[] items = line.Split(',');
+                Sale sale = new Sale {//p105 オブジェクトの初期化
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+        //店舗別売り上げを求める
+        public IDictionary<String, int> GetPerProductSales() {
+            var dict = new Dictionary<String, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ProductCategory))
+                    dict[sale.ProductCategory] += sale.Amount;
+                else
+                    dict[sale.ProductCategory] = sale.Amount;
+            }
+            return dict;
+        }
+    }
+}
