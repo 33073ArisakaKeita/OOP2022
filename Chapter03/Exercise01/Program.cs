2103274cc29be6f6374e586730139a74b18d66ed
33073有阪圭太 <infosys@C4210>
2022-05-17 15:44:42 +0900

    Exercise1_3 1_4

--- Chapter03/Exercise01/Program.cs
+++ Chapter03/Exercise01/Program.cs
@@ -14,6 +14,25 @@ namespace Exercise01 {
 
             Exercise1_2(numbers);
             Console.WriteLine("---------------");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("---------------");
+
+            Exercise1_4(numbers);
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+            var list = numbers.Select(n => n * 2);
+            numbers[5] = 5000;
+            foreach (var n in list) {//遅延実行(即時実行)
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+            foreach (var n in numbers.Where(n => n >= 50)) {
+                Console.WriteLine(n);
+            }
         }
 
         private static void Exercise1_1(List<int> numbers) {
