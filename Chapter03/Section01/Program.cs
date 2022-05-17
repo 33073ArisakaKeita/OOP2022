fbc46263da749b396d3445c0a141189ba133bfbc
33073有阪圭太 <infosys@C4210>
2022-05-17 15:45:52 +0900

    FindAll RemoveAll等々

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -11,9 +11,19 @@ namespace Section01 {
             var list = new List<string> {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+            //FindAllメソッド
+            //var names = list.FindAll(s => s.Length <= 5);
+            //names.ForEach(s => Console.WriteLine(s));
 
-            var names = list.FindAll(s => s.Length <= 5);
-            names.ForEach(s => Console.WriteLine(s));
+            //list.ForEach(s => Console.WriteLine(s));
+
+            //RemoveAllメソッド
+            //var removedCount = list.RemoveAll(s => s.Contains("on"));
+            //Console.WriteLine(removedCount);
+            //list.ForEach(s => Console.WriteLine(s));
+
+            var lowerList = list.ConvertAll(s => s.ToLower());
+            lowerList.ForEach(s => Console.WriteLine(s));
 
 
             //foreach(var s in names) {
