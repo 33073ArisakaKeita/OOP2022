34779a9ddcec63352ea8535c2871cbc7744face6
33073有阪圭太 <infosys@C4210>
2022-05-20 15:55:41 +0900

    問題4-1　完成

---
+++ Chapter04/Exercise01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var ym = new YearMonth(2017, 8);
+            var test =  ym.AddOneMonth();
+            var check = ym.Is21Century;
+            Console.WriteLine(ym);
+        }
+    }
+}
