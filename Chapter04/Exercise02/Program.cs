a5881cf329a6f86119c277c091b8f163a1bb4577
33073有阪圭太 <infosys@C4210>
2022-05-24 15:37:16 +0900

    4.2.1～4.2.4　完成

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -8,10 +8,59 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-            //var ym = new YearMonth(2017, 8);
-            //var test = ym.AddOneMonth();
-            //var check = ym.Is21Century;
-            //Console.WriteLine(ym);
+            //4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1900,3),
+                new YearMonth(1999,4),
+                new YearMonth(2010,5),
+                new YearMonth(2000,7),
+                new YearMonth(1000,8),
+            };
+
+            //4.2.2
+            Exercise2_2(ymCollection);
+            Console.WriteLine("-----");
+
+            //4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("-----");
+
+            //4.2.5
+            Exercise2_5(ymCollection);
+            Console.WriteLine("-----");
+        }
+        //4.2.3
+            static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var ym in yms) {
+                if (ym.Is21Century) {
+                    return ym;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var _21c = FindFirst21C(ymCollection);
+            if (_21c != null)
+                Console.WriteLine(_21c);
+            else
+                Console.WriteLine("21世紀のデータはありません");
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var nextMonth = new YearMonth[ymCollection.Length];
+            int i = 0;
+            foreach(var ym in ymCollection) {
+                nextMonth[i] = ym.AddOneMonth();
+                i++;
+            }
+            Exercise2_2(nextMonth);
         }
     }
 }
