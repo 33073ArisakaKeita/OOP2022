3300fec4324ec07771e87f2338b5de8eed3321af
33073有阪圭太 <infosys@C4210>
2022-05-26 14:54:36 +0900

    閏年等々

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -11,10 +11,12 @@ namespace Exercise02 {
             //4.2.1
             var ymCollection = new YearMonth[] {
                 new YearMonth(1900,3),
-                new YearMonth(1999,4),
-                new YearMonth(2010,5),
-                new YearMonth(2000,7),
-                new YearMonth(1000,8),
+                new YearMonth(1940,4),
+                new YearMonth(2012,5),
+                new YearMonth(1940,9),
+                new YearMonth(1948,8),
+                new YearMonth(1900,8),
+                new YearMonth(2000,8),
             };
 
             //4.2.2
@@ -34,10 +36,25 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_6(YearMonth[] ymCollection) {
-            foreach (var ym in ymCollection.OrderByDescending(ym => ym.Year)) {
-                Console.WriteLine(ym);
-            }
+            //②偶数年のみ昇順に出力
+            //foreach (var ym in ymCollection.Where(ym => ym.Year % 2 == 0)
+            //                               .OrderBy(ym => ym.Year)){
+            //    Console.WriteLine(ym);
+            //}
+            //③閏年のみ出力
+            //foreach (var ym in ymCollection.Where(ym => (ym.Year % 4 == 0 && ym.Year %100 !=0 || ym.Year % 400 == 0)))
+            //                     {
+            //    Console.WriteLine(ym);
+            //}
+            //foreach (var ym in ymCollection.Where(ym => DateTime.IsLeapYear(ym.Year))) {
+            //    Console.WriteLine(ym);
+            //}
+            //④全ての年から12月に一番近い月を出力
+            var deta = ymCollection.Max(ym => ym.Month); 
+            Console.WriteLine(deta);
             
+
+
         }
 
         //4.2.3
