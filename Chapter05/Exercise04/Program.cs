b6b3b17771ae6180d5de0d574ef4d8b67d5c5c71
33073有阪圭太 <infosys@C4210>
2022-05-27 15:53:34 +0900

    Exercise04 完成

--- Chapter05/Exercise04/Program.cs
+++ Chapter05/Exercise04/Program.cs
@@ -8,7 +8,24 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var text = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            string[] words = text.Split(';');
             
+            foreach(var a in words) {
+                string[] b = a.Split('=');
+                Console.WriteLine("{0}:{1}",ToJapanese(b[0]),b[1]);
+            }
+            
+        }
+        static string ToJapanese(string key) {
+            switch (key) {
+                case "Novelist":
+                    return "作家　";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+            }
+            throw new ArgumentException("引数keyは、正しい値ではありません");
         }
     }
 }
