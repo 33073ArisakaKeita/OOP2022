b245bddf5d148d517c99ff8424caa04cf29bc8d7
33073有阪圭太 <infosys@C4210>
2022-05-27 13:52:55 +0900

    3-1,3-2,3-3　完成

--- Chapter05/Exercise03/Program.cs
+++ Chapter05/Exercise03/Program.cs
@@ -9,7 +9,7 @@ namespace Exercise03 {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
 
-            //Exercise3_1(text);
+            Exercise3_1(text);
             Console.WriteLine("---------------");
 
             Exercise3_2(text);
@@ -25,22 +25,33 @@ namespace Exercise03 {
             Console.WriteLine("---------------");
         }
 
-        private static void Exercise3_1(object text) {
+        private static void Exercise3_1(String text) {
+            var count = text.Where(c => c == ' ')
+                            .Count();
+            Console.WriteLine("空欄の数は{0}個", count);
+
+            //foreach(var c in text) {
+            //    if(c == ' ') {
+            //        count++;
+            //    }
+            //}
         }
 
-        private static void Exercise3_2(object text) {
-            throw new NotImplementedException();
+        private static void Exercise3_2(String text) {
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
         }
 
-        private static void Exercise3_3(object text) {
-            throw new NotImplementedException();
+        private static void Exercise3_3(String text) {
+            String[] words = text.Split(' ');
+            Console.WriteLine("テキストに含まれる単語数は{0}個", words.Length);
         }
 
-        private static void Exercise3_4(object text) {
-            throw new NotImplementedException();
+        private static void Exercise3_4(String text) {
+            
         }
 
-        private static void Exercise3_5(object text) {
+        private static void Exercise3_5(String text) {
             throw new NotImplementedException();
         }
     }
