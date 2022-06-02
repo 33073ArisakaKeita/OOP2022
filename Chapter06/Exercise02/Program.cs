b4e3bdd0149c0845e521014f55af334de6308b8d
33073有阪圭太 <infosys@C4210>
2022-06-02 14:02:42 +0900

    Exercise2_1

--- Chapter06/Exercise02/Program.cs
+++ Chapter06/Exercise02/Program.cs
@@ -43,10 +43,12 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_2(List<Book> books) {
-            
+            var count = books.Count(s => s.Title.Contains("C#"));
+            Console.WriteLine(count);
         }
 
         private static void Exercise2_3(List<Book> books) {
+
         }
 
         private static void Exercise2_4(List<Book> books) {
