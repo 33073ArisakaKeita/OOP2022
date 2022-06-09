8404b74abb2358bd26e9fc439985c01fbf3229ef
33073有阪圭太 <infosys@C4210>
2022-06-09 13:37:13 +0900

    数当てゲーム ValueChanged

---
+++ FormAppSample/NumberGame/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace NumberGame {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Number = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maxNum = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Number)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HGS創英角ｺﾞｼｯｸUB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(207, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1～";
+            // 
+            // Number
+            // 
+            this.Number.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Number.Location = new System.Drawing.Point(179, 242);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(168, 55);
+            this.Number.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
+            this.button1.Font = new System.Drawing.Font("HGS創英角ｺﾞｼｯｸUB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(509, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 76);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "入力";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("HGS創英角ｺﾞｼｯｸUB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(104, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 48);
+            this.label2.TabIndex = 0;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // maxNum
+            // 
+            this.maxNum.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.maxNum.Location = new System.Drawing.Point(324, 102);
+            this.maxNum.Name = "maxNum";
+            this.maxNum.Size = new System.Drawing.Size(168, 55);
+            this.maxNum.TabIndex = 1;
+            this.maxNum.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.maxNum.ValueChanged += new System.EventHandler(this.maxNum_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("HGS創英角ｺﾞｼｯｸUB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(501, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(356, 48);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "の中から以下略";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(869, 486);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maxNum);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "数当てゲーム";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Number)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown Number;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown maxNum;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FormAppSample/NumberGame/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGame {
+    public partial class Form1 : Form {
+        Random rnd = new Random();//乱数オブジェクト
+        int ans;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (Number.Value == ans){
+                label2.Text = "正解！";
+            }
+            else if(Math.Abs(ans-Number.Value)<3){
+                label2.Text = "惜しい!";
+            }
+            else if ((ans - Number.Value) < 0){
+                label2.Text = "答えはもっと小さい!!!!!!!!!!!";
+            }
+            else {
+                label2.Text = "答えはもっと大きい!!!!!!!!!!!";
+            }
+        }
+ 
+        private void Form1_Load(object sender, EventArgs e) {
+            ans = rnd.Next(1, (int)maxNum.Value);
+        }
+
+        private void maxNum_ValueChanged(object sender, EventArgs e) {
+            ans = rnd.Next(1, (int)maxNum.Value);
+        }
+    }
+}
