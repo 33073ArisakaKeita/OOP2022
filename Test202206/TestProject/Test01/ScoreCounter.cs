b2bc97ecfdaae6046fe09624fb3ccfa0bc1c1e81
33073有阪圭太 <infosys@C4210>
2022-06-14 15:48:41 +0900

    設問1　コメント追加

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -3,39 +3,40 @@ using System.IO;
 
 namespace Test01 {
     class ScoreCounter {
-        private IEnumerable<Student> _score;
+        private IEnumerable<Student> _score;//ファイルから読み込んだデータ
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
             _score = ReadScore(filePath);
         }
 
-        //メソッドの概要： データを読み込み、Studentオブジェクトのリストを返す
+        //メソッドの概要：データを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
             var students = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            foreach (var line in lines) {
-                var items = line.Split(',');
-                var student = new Student {
+            foreach (var line in lines) {//一行ずつ処理
+                var items = line.Split(',');//「,」で分割
+                var student = new Student {//オブジェクトの初期化
                     Name = items[0],
                     Subject = items[1],
                     Score = int.Parse(items[2])
                 };
-                students.Add(student);
+                students.Add(student);//Listに追加する
             }
-            return students;
+            return students;//Listを返す
         }
 
-        //メソッドの概要： 科目別の点数を集計
+        //メソッドの概要：科目別の点数を集計
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
             foreach (var score in _score) {
+                //ContainsKeyでdictに指定したSubjectが格納してあるか否か
                 if (dict.ContainsKey(score.Subject))
                     dict[score.Subject] += score.Score;
                 else
                     dict[score.Subject] = score.Score;
             }
-            return dict;
+            return dict;//結果を返す
         }
     }
 }
