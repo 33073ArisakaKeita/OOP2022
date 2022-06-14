1cc1c0348b6a8df27ce33a5789d4c82cdb7ba90e
33073有阪圭太 <infosys@C4210>
2022-06-14 14:29:47 +0900

    途中経過

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 using System.IO;
 
 namespace Test01 {
@@ -7,23 +8,31 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            List<Student> sales = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                var student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                student.Add(student);
+            }
+            return student;
 
 
 
 
 
 
-            
+
         }
 
         //メソッドの概要： 
