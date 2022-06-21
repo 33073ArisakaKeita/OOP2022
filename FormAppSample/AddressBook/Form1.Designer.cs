465d678a70d5500d9d6aac8a8be34ca994c58b5b
33073有阪圭太 <infosys@C4210>
2022-06-21 14:27:22 +0900

    dateTimePicker　追加

--- FormAppSample/AddressBook/Form1.Designer.cs
+++ FormAppSample/AddressBook/Form1.Designer.cs
@@ -48,6 +48,8 @@ namespace AddressBook {
             this.btSave = new System.Windows.Forms.Button();
             this.btOpen = new System.Windows.Forms.Button();
             this.sfdSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
             this.SuspendLayout();
@@ -288,11 +290,30 @@ namespace AddressBook {
             this.btOpen.UseVisualStyleBackColor = true;
             this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(473, 516);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "登録日：";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(531, 514);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 19);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(761, 542);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.cbCompany);
             this.Controls.Add(this.btPictureClear);
             this.Controls.Add(this.btPictureOpen);
@@ -351,6 +372,8 @@ namespace AddressBook {
         private System.Windows.Forms.Button btSave;
         private System.Windows.Forms.Button btOpen;
         private System.Windows.Forms.SaveFileDialog sfdSaveDialog;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- FormAppSample/AddressBook/Form1.cs
+++ FormAppSample/AddressBook/Form1.cs
@@ -19,11 +19,7 @@ namespace AddressBook {
             InitializeComponent();
             dgvPersons.DataSource = listPerson;
         }
-        private void btPictureOpen_Click(object sender, EventArgs e) {
-            if(ofdFileOpenDialog.ShowDialog() == DialogResult.OK) {
-                pbPicture.Image = Image.FromFile(ofdFileOpenDialog.FileName);
-            }
-        }
+
         private void btAddPerson_Click(object sender, EventArgs e) {
             if (String.IsNullOrWhiteSpace(tbName.Text)) {
                 MessageBox.Show("氏名が入力されていません");
@@ -37,6 +33,7 @@ namespace AddressBook {
                     Company = cbCompany.Text,
                     Picture = pbPicture.Image,
                     listGroup = GetCheckBoxGroup(),
+                    Registration = dateTimePicker1.Value,
                 };
                 listPerson.Add(newPerson);
                 this.dgvPersons.CurrentCell = this.dgvPersons[0, listPerson.Count - 1];
@@ -47,65 +44,7 @@ namespace AddressBook {
                 CheckBoxClear();
             }
         }
-        private void setCbCompany(string company) {
-            if (!cbCompany.Items.Contains(company)) {
-                cbCompany.Items.Add(company);
-            }
-        }
-        //チェックボックスにセットされている値をリストとして取り出す
-        private List<Person.GroupType> GetCheckBoxGroup() {
-            var listGroup = new List<Person.GroupType>();
-            if (cbFamily.Checked)
-                listGroup.Add(Person.GroupType.家族);
-            if (cbFriend.Checked)
-                listGroup.Add(Person.GroupType.友人);
-            if (cbWork.Checked)
-                listGroup.Add(Person.GroupType.仕事);
-            if (cbOther.Checked)
-                listGroup.Add(Person.GroupType.その他);
-            return listGroup;
-        }
-        private void btPictureClear_Click(object sender, EventArgs e) {
-            pbPicture.Image = null;
-        }
-        //データグリッドビューをクリックしたときのイベントハンドラ
-        private void dgvPersons_Click(object sender, EventArgs e) {
-            //選択されているインデックスを取得する
-            if (dgvPersons.CurrentRow == null) return;
 
-            int index = dgvPersons.CurrentCell.RowIndex;
-            //各リストの項目をテキストボックスへ表示する
-            tbName.Text = listPerson[index].Name;
-            tbMailAddress.Text = listPerson[index].MailAddress;
-            tbAddress.Text = listPerson[index].Address;
-            cbCompany.Text = listPerson[index].Company;
-            pbPicture.Image = listPerson[index].Picture;
-            CheckBoxClear();
-            CheckBoxCheck(index);
-        }
-        private void CheckBoxCheck(int index) {
-            foreach (var group in listPerson[index].listGroup) {
-                switch (group) {
-                    case Person.GroupType.家族:
-                        cbFamily.Checked = true;
-                        break;
-                    case Person.GroupType.友人:
-                        cbFriend.Checked = true;
-                        break;
-                    case Person.GroupType.仕事:
-                        cbWork.Checked = true;
-                        break;
-                    case Person.GroupType.その他:
-                        cbOther.Checked = true;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        private void CheckBoxClear() {
-            cbFamily.Checked = cbFriend.Checked = cbWork.Checked = cbOther.Checked = false;
-        }
         private void btUpdate_Click(object sender, EventArgs e) {
             if (dgvPersons.CurrentRow == null) return;
             int index = dgvPersons.CurrentCell.RowIndex;
@@ -116,8 +55,11 @@ namespace AddressBook {
             listPerson[index].Company = cbCompany.Text;
             listPerson[index].Picture = pbPicture.Image;
             listPerson[index].listGroup = GetCheckBoxGroup();
+            listPerson[index].Registration = dateTimePicker1.Value;
+
             dgvPersons.Refresh();
         }
+
         private void btdelete_Click(object sender, EventArgs e) {
             if (dgvPersons.CurrentRow == null) return;
             int index = dgvPersons.CurrentCell.RowIndex;
@@ -140,27 +82,23 @@ namespace AddressBook {
             }
             dgvPersons.Refresh();
         }
-        private void tbNull() {
-            tbName.Text = null;
-            tbMailAddress.Text = null;
-            tbAddress.Text = null;
-            cbCompany.Text = null;
+
+        private void btPictureOpen_Click(object sender, EventArgs e) {
+            if (ofdFileOpenDialog.ShowDialog() == DialogResult.OK) {
+                pbPicture.Image = Image.FromFile(ofdFileOpenDialog.FileName);
+            }
+        }
+
+        private void btPictureClear_Click(object sender, EventArgs e) {
             pbPicture.Image = null;
         }
-        private void Form1_Load(object sender, EventArgs e) {   
-            EnabledCheck();  
-        }
-        private void EnabledCheck() {
-            //三項条件演算子
-            btDelete.Enabled = btUpdate.Enabled
-                = listPerson.Count() > 0 ? true : false;
-        }
+
         private void btSave_Click(object sender, EventArgs e) {
             if (sfdSaveDialog.ShowDialog() == DialogResult.OK) {
                 try {
                     //バイナリ形式でシリアル化
                     var bf = new BinaryFormatter();
-                    using (FileStream fs = File.Open(sfdSaveDialog.FileName,FileMode.Create)) {
+                    using (FileStream fs = File.Open(sfdSaveDialog.FileName, FileMode.Create)) {
                         bf.Serialize(fs, listPerson);
                     }
                 }
@@ -169,6 +107,7 @@ namespace AddressBook {
                 }
             }
         }
+
         private void btOpen_Click(object sender, EventArgs e) {
             if (ofdFileOpenDialog.ShowDialog() == DialogResult.OK) {
                 try {
@@ -185,11 +124,92 @@ namespace AddressBook {
                 catch (Exception ex) {
                     MessageBox.Show(ex.Message);
                 }
+                cbCompany.Items.Clear();
                 foreach (var company in listPerson.Select(p => p.Company)) {
                     setCbCompany(company);//存在する会社を登録
                 }
             }
             EnabledCheck();
+        }
+
+        //データグリッドビューをクリックしたときのイベントハンドラ
+        private void dgvPersons_Click(object sender, EventArgs e) {
+            //選択されているインデックスを取得する
+            if (dgvPersons.CurrentRow == null) return;
+
+            int index = dgvPersons.CurrentCell.RowIndex;
+            //各リストの項目をテキストボックスへ表示する
+            tbName.Text = listPerson[index].Name;
+            tbMailAddress.Text = listPerson[index].MailAddress;
+            tbAddress.Text = listPerson[index].Address;
+            cbCompany.Text = listPerson[index].Company;
+            pbPicture.Image = listPerson[index].Picture;
+            dateTimePicker1.Value = listPerson[index].Registration;
+            CheckBoxClear();
+            CheckBoxCheck(index);
+        }
+
+        //チェックボックスにセットされている値をリストとして取り出す
+        private List<Person.GroupType> GetCheckBoxGroup() {
+            var listGroup = new List<Person.GroupType>();
+            if (cbFamily.Checked)
+                listGroup.Add(Person.GroupType.家族);
+            if (cbFriend.Checked)
+                listGroup.Add(Person.GroupType.友人);
+            if (cbWork.Checked)
+                listGroup.Add(Person.GroupType.仕事);
+            if (cbOther.Checked)
+                listGroup.Add(Person.GroupType.その他);
+            return listGroup;
+        }
+
+        private void CheckBoxCheck(int index) {
+            foreach (var group in listPerson[index].listGroup) {
+                switch (group) {
+                    case Person.GroupType.家族:
+                        cbFamily.Checked = true;
+                        break;
+                    case Person.GroupType.友人:
+                        cbFriend.Checked = true;
+                        break;
+                    case Person.GroupType.仕事:
+                        cbWork.Checked = true;
+                        break;
+                    case Person.GroupType.その他:
+                        cbOther.Checked = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void CheckBoxClear() {
+            cbFamily.Checked = cbFriend.Checked = cbWork.Checked = cbOther.Checked = false;
+        }
+
+        private void tbNull() {
+            tbName.Text = null;
+            tbMailAddress.Text = null;
+            tbAddress.Text = null;
+            cbCompany.Text = null;
+            pbPicture.Image = null;
+        }
+
+        private void setCbCompany(string company) {
+            if (!cbCompany.Items.Contains(company)) {
+                //まだ登録されていなければ登録
+                cbCompany.Items.Add(company);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {   
+            EnabledCheck();  
+        }
+
+        private void EnabledCheck() {
+            //三項条件演算子
+            btDelete.Enabled = btUpdate.Enabled = listPerson.Count() > 0 ? true : false;
         }
     }
 }
