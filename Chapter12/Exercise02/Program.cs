5802575582ff5fdedc93ca8d4b4c1da442261386
33073有阪圭太 <infosys@C4210>
2022-07-12 14:14:10 +0900

    Exercise2-1 2-2　完成

--- Chapter12/Exercise02/Program.cs
+++ Chapter12/Exercise02/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -10,32 +12,39 @@ using System.Xml.Serialization;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-            //var novelist = Exercise2_1("Sample.xml");
-            //Exercise2_2(novelist, "novelist.json");
+            var novelist = Exercise2_1("Sample.xml");
+            Exercise2_2(novelist, "novelist.json");
 
-           // // これは確認のためのコード 12.2.1
-            //Console.WriteLine("{0} {1}", novelist.Name, novelist.Birth);
-            //foreach (var title in novelist.Masterpieces) {
-            //    Console.WriteLine(title);
-            //}
-            //Console.WriteLine();
+            // これは確認のためのコード 12.2.1
+            Console.WriteLine("{0} {1}", novelist.Name, novelist.Birth);
+            foreach (var title in novelist.Masterpieces) {
+                Console.WriteLine(title);
+            }
+            Console.WriteLine();
 
-            //// これは確認のためのコード 12.2.2
-            //Console.WriteLine(File.ReadAllText("novelist.json"));
-            //Console.WriteLine();
+            // これは確認のためのコード 12.2.2
+            Console.WriteLine(File.ReadAllText("novelist.json"));
+            Console.WriteLine();
         }
 
-        //private static Novelist Exercise2_1(string file) {
+        private static Novelist Exercise2_1(string file) {
             //引数で受け取ったファイルを逆シリアル化しNovelistクラスのオブジェクトにセットする
-            //using (var reader = XmlReader.Create(file)) {
-            //    var serializer = new XmlSerializer(typeof(Novelist));
-            //    var novelist = serializer.Deserialize(reader) as Novelist;
-            //    //オブジェクトをreturn
-            //    return novelist;
-            //}
-        //}
+            using (var reader = XmlReader.Create(file)) {
+                var serializer = new XmlSerializer(typeof(Novelist));
+                var novelist = serializer.Deserialize(reader) as Novelist;
+                //オブジェクトをreturn
+                return novelist;
+            }
+        }
 
         private static void Exercise2_2(Novelist novelist, string file) {
+            using (var stream = new FileStream(file, FileMode.Create,
+                                               FileAccess.Write)) {
+                var serializer = new DataContractJsonSerializer(novelist.GetType(), new DataContractJsonSerializerSettings {
+                    DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                });
+                serializer.WriteObject(stream, novelist);
+            }
         }
     }
 }
