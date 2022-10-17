737a928a3f5e1f56a6e0be3dd8bef6847d1f1a9d
33073有阪圭太 <infosys@C4210>
2022-10-17 15:57:37 +0900

    数当て

---
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    ///
+    
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        static Random r = new Random();
+        int a = r.Next(1, 25);
+
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            
+            Button bt = (Button)sender;
+            if (a == int.Parse((string)bt.Content)) {
+                textblock.Text = "正解";
+                bt.Background = Brushes.Red;
+            }
+                
+            if(a < int.Parse((string)bt.Content))
+                textblock.Text = "もっと小さい";
+            if (a > int.Parse((string)bt.Content))
+                textblock.Text = "もっと大きい";
+        }
+    }
+}
--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            seasonComboBox.SelectedIndex = 0;
+            seasonComboBox.SelectedIndex = GetSeason();
         }
 
         private void checkBox_Checked(object sender, RoutedEventArgs e) {
@@ -48,6 +48,45 @@ namespace SampleApplication {
 
         private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
+        private int GetSeason() {
+            int month = (int)DateTime.Now.Month;
+            if (3 <= month & month <= 5)
+                return 0;
+            if (6 <= month & month <= 8)
+                return 1;
+            if (9 <= month & month <= 11)
+                return 2;
+            else
+                return 3;
+
+            //int ret = 0;
+            //switch (month) {
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        ret = 0;
+            //        break;
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        ret = 1;
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        ret = 2;
+            //        break;
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        ret = 3;
+            //        break;
+            //    default:
+            //        break;
+            //}
+            //return ret;
         }
     }
 }
