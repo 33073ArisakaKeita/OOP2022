5d88c4ebccbff3df73c4d1b886a66fd27ff9b3a5
33073有阪圭太 <infosys@C4210>
2022-10-27 11:05:27 +0900

    ある程度完成

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,10 +21,45 @@ namespace CollarChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList(); //←追加
+
         }
 
         private void SampleSlider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            Null_Check();
             color_label.Background = new SolidColorBrush(Color.FromRgb((byte)int.Parse(textbox_R.Text), (byte)int.Parse(textbox_G.Text), (byte)int.Parse(textbox_B.Text)));
+        }
+
+        private void textbox_R_KeyUp(object sender, KeyEventArgs e) {
+            Null_Check();
+            color_label.Background = new SolidColorBrush(Color.FromRgb((byte)int.Parse(textbox_R.Text), (byte)int.Parse(textbox_G.Text), (byte)int.Parse(textbox_B.Text)));
+
+        }
+        private void Null_Check() {
+            if (String.IsNullOrEmpty(textbox_R.Text))
+                textbox_R.Text = "0";
+            if (String.IsNullOrEmpty(textbox_G.Text))
+                textbox_G.Text = "0";
+            if (String.IsNullOrEmpty(textbox_B.Text))
+                textbox_B.Text = "0";
+        }
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+        public class MyColor {
+            public Color Color { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            SampleSlider_R.Value = color.R;
+            SampleSlider_G.Value = color.G;
+            SampleSlider_B.Value = color.B;
+            color_label.Background = new SolidColorBrush(Color.FromRgb(color.R, color.G, color.B));
+
         }
     }
 }
