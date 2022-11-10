0d6a3a75c22e6fb9d92169a8f6e4745a9565196f
33073有阪圭太 <infosys@C4210>
2022-11-10 15:58:11 +0900

    途中。先が見えないので多分作りを変える

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbWeatherInfo = new System.Windows.Forms.TextBox();
+            this.btWeatherGet = new System.Windows.Forms.Button();
+            this.cbtihou1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbtihou2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // tbWeatherInfo
+            // 
+            this.tbWeatherInfo.Location = new System.Drawing.Point(12, 186);
+            this.tbWeatherInfo.Multiline = true;
+            this.tbWeatherInfo.Name = "tbWeatherInfo";
+            this.tbWeatherInfo.Size = new System.Drawing.Size(541, 252);
+            this.tbWeatherInfo.TabIndex = 0;
+            // 
+            // btWeatherGet
+            // 
+            this.btWeatherGet.Location = new System.Drawing.Point(171, 73);
+            this.btWeatherGet.Name = "btWeatherGet";
+            this.btWeatherGet.Size = new System.Drawing.Size(75, 23);
+            this.btWeatherGet.TabIndex = 1;
+            this.btWeatherGet.Text = "取得";
+            this.btWeatherGet.UseVisualStyleBackColor = true;
+            this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
+            // 
+            // cbtihou1
+            // 
+            this.cbtihou1.FormattingEnabled = true;
+            this.cbtihou1.Location = new System.Drawing.Point(12, 24);
+            this.cbtihou1.Name = "cbtihou1";
+            this.cbtihou1.Size = new System.Drawing.Size(121, 20);
+            this.cbtihou1.TabIndex = 2;
+            this.cbtihou1.SelectedIndexChanged += new System.EventHandler(this.cbtihou1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "地方";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "県";
+            // 
+            // cbtihou2
+            // 
+            this.cbtihou2.FormattingEnabled = true;
+            this.cbtihou2.Location = new System.Drawing.Point(14, 86);
+            this.cbtihou2.Name = "cbtihou2";
+            this.cbtihou2.Size = new System.Drawing.Size(121, 20);
+            this.cbtihou2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbtihou2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbtihou1);
+            this.Controls.Add(this.btWeatherGet);
+            this.Controls.Add(this.tbWeatherInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWeatherInfo;
+        private System.Windows.Forms.Button btWeatherGet;
+        private System.Windows.Forms.ComboBox cbtihou1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbtihou2;
+    }
+}
+
---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WeatherApp.jma;
+
+//salescalcで出来る
+
+namespace WeatherApp {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            cbtihou1.Items.Add("北海道地方");
+            cbtihou1.Items.Add("東北地方");
+            cbtihou1.Items.Add("関東甲信地方");
+            cbtihou1.Items.Add("東海地方");
+            cbtihou1.Items.Add("北陸地方");
+            cbtihou1.Items.Add("近畿地方");
+            cbtihou1.Items.Add("中国地方（山形県を除く）地方");
+            cbtihou1.Items.Add("四国地方");
+            cbtihou1.Items.Add("九州北部(山形県を含む)地方");
+            cbtihou1.Items.Add("九州南部・奄美地方");
+            cbtihou1.Items.Add("沖縄地方");
+
+            var reagionDict = new Dictionary<string, int>() {
+
+            }
+        }
+
+        private void btWeatherGet_Click(object sender, EventArgs e) {
+            var wc = new WebClient() {
+                Encoding = Encoding.UTF8
+            };
+
+            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json");
+
+            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+
+            tbWeatherInfo.Text += json.text;
+
+        }
+
+        private void cbtihou1_SelectedIndexChanged(object sender, EventArgs e) {
+            var index = cbtihou1.SelectedIndex;
+            if (index == -1)
+                return;
+            cbtihou2.Items.Clear();
+            switch (index) {
+                case 0:
+                    cbtihou2.Items.Add("宗谷地方");
+                    cbtihou2.Items.Add("上川・留萌地方");
+                    cbtihou2.Items.Add("網走・北見・紋別地方");
+                    cbtihou2.Items.Add("十勝地方");
+                    cbtihou2.Items.Add("釧路・根室地方");
+                    cbtihou2.Items.Add("胆振・日高地方");
+                    cbtihou2.Items.Add("石狩・空知・後志地方");
+                    cbtihou2.Items.Add("渡島・檜山地方");
+                    break;
+                case 2:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 3:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 4:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 5:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 6:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 7:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 8:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 9:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 10:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                case 1:
+                    cbtihou2.Items.Add("地方");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
